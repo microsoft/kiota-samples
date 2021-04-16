c223dd08a779a55c792010dc294a2281f84621ea
Vincent Biret <vibiret@microsoft.com>
2021-04-16 12:05:09 -0400

    - adds a template for new samples

---
+++ template/README.md
@@ -0,0 +1,9 @@
+# New samples template
+
+The following section is a set of template projects that can be used to create a new sample.
+
+## Table of content
+
+- [dotnet](./dotnet)
+- [java](./java)
+- [typescript](./typescript)
---
+++ template/dotnet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Graphdotnetv4
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Write("Hello world!");
+        }
+    }
+}
