31c30163b720ca6fa44c9e4aafae68173a80882a
Vincent Biret <vibiret@microsoft.com>
2021-07-07 14:28:57 -0400

    - adds support for serialization module loading in typescript

--- msgraph-mail/typescript/src/apiClient.ts
+++ msgraph-mail/typescript/src/apiClient.ts
@@ -1,6 +1,6 @@
 import {UserRequestBuilder} from './users/item/userRequestBuilder';
 import {UsersRequestBuilder} from './users/usersRequestBuilder';
-import {HttpCore, HttpMethod, RequestInfo, ResponseHandler, SerializationWriterFactory} from '@microsoft/kiota-abstractions';
+import {HttpCore, HttpMethod, RequestInfo, ResponseHandler, SerializationWriterFactory, registerDefaultSerializers, registerDefaultDeserializers, enableBackingStore, SerializationWriterFactoryRegistry, ParseNodeFactoryRegistry} from '@microsoft/kiota-abstractions';
 
 /** The main entry point of the SDK, exposes the configuration and the fluent API.  */
 export class ApiClient {
@@ -10,6 +10,8 @@ export class ApiClient {
     public httpCore?: HttpCore | undefined;
     /** Path segment to use to build the URL for the current request builder  */
     private readonly pathSegment: string;
+    /** Promise to await/call then on for client initialization to be completed.  */
+    public readonly registrationMemoize: Promise<void>;
     /** Factory to use to get a serializer for payload serialization  */
     public serializerFactory?: SerializationWriterFactory | undefined;
     public get users(): UsersRequestBuilder {
@@ -20,10 +22,19 @@ export class ApiClient {
         return builder;
     }
     /**
-     * Instantiates a new ApiClient and sets the default values.
+     * Instantiates a new Api client and sets the default values.
+     * @param httpCore The http core service to use to execute the requests.
+     * @param serializationWriterFactory Factory to use to get a serializer for payload serialization.
      */
-    public constructor() {
+    public constructor(httpCore: HttpCore | undefined, serializationWriterFactory?: SerializationWriterFactory | undefined) {
+        if(!httpCore) throw new Error("httpCore cannot be undefined");
         this.pathSegment = "https://graph.microsoft.com/v1.0";
+        httpCore = httpCore;
+        this.registrationMemoize = Promise.all([registerDefaultSerializers("@microsoft/kiota-serialization-json/JsonSerializationWriterFactory")]).then(() => Promise.all([registerDefaultDeserializers("@microsoft/kiota-serialization-json/JsonParseNodeFactory")])).then(() => {
+            if(!serializationWriterFactory && SerializationWriterFactoryRegistry.defaultInstance.contentTypeAssociatedFactories.size === 0) throw new Error("The Serialization Writer factory has not been initialized for this client.");
+            if(!serializationWriterFactory && ParseNodeFactoryRegistry.defaultInstance.contentTypeAssociatedFactories.size === 0) throw new Error("The Parse Node factory has not been initialized for this client.");
+            this.serializerFactory = serializationWriterFactory ?? SerializationWriterFactoryRegistry.defaultInstance;
+        });
     };
     /**
      * Gets an item from the graphtypescriptv4.utilities.users collection
