9a328303dc195451ccb5985057819ef6eac2b443
Vincent Biret <vibiret@microsoft.com>
2021-07-12 13:35:39 -0400

    - code-gen update: removes uncessary property on api client in dotnet

--- msgraph-mail/dotnet/ApiClient.cs
+++ msgraph-mail/dotnet/ApiClient.cs
@@ -10,14 +10,12 @@ using System.Threading.Tasks;
 namespace Graphdotnetv4 {
     /// <summary>The main entry point of the SDK, exposes the configuration and the fluent API.</summary>
     public class ApiClient {
-        /// <summary>Current path for the request</summary>
-        private string CurrentPath { get; set; }
         /// <summary>The http core service to use to execute the requests.</summary>
         private IHttpCore HttpCore { get; set; }
         /// <summary>Path segment to use to build the URL for the current request builder</summary>
         private string PathSegment { get; set; }
         public UsersRequestBuilder Users { get =>
-            new UsersRequestBuilder(CurrentPath + PathSegment , HttpCore);
+            new UsersRequestBuilder(PathSegment , HttpCore);
         }
         /// <summary>
         /// Instantiates a new ApiClient and sets the default values.
