feb07d776872eca4a6dd39aea5387d139324010d
Vincent Biret <vibiret@microsoft.com>
2021-08-06 10:02:23 -0400

    - code-gen: go primitive types http core fix

--- msgraph-mail/go/utilities/users/messages/content/content_request_builder.go
+++ msgraph-mail/go/utilities/users/messages/content/content_request_builder.go
@@ -53,7 +53,7 @@ func (m *ContentRequestBuilder) Get(h func (value map[string]string) (err error)
         return func() ([]byte, error) { return nil, err }
     }
     return func() ([]byte, error) {
-        res, err := m.httpCore.SendAsync(*requestInfo, func () i04eb5309aeaafadd28374d79c8471df9b267510b4dc2e3144c378c50f6fd7b55.Parsable { return new([]byte) }, *responseHandler)()
+        res, err := m.httpCore.SendPrimitiveAsync(*requestInfo, "[]byte", *responseHandler)()
         if err != nil {
             return nil, err
         }
