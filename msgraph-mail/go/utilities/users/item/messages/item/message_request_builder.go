a123d96dc196da9de8df058c8865acb14b2f13f8
Vincent Biret <vibiret@microsoft.com>
2021-11-05 14:11:35 -0400

    - code-gen: go support for base url

--- msgraph-mail/go/utilities/users/item/messages/item/message_request_builder.go
+++ msgraph-mail/go/utilities/users/item/messages/item/message_request_builder.go
@@ -84,7 +84,7 @@ func (m *MessageRequestBuilder) AttachmentsById(id string)(*idd4800ce8115a136eb3
 func NewMessageRequestBuilderInternal(pathParameters map[string]string, requestAdapter ida96af0f171bb75f894a4013a6b3146a4397c58f11adb81a2b7cbea9314783a9.RequestAdapter)(*MessageRequestBuilder) {
     m := &MessageRequestBuilder{
     }
-    m.urlTemplate = "https://graph.microsoft.com/v1.0/users/{user_id}/messages/{message_id}{?select}";
+    m.urlTemplate = "{+baseurl}/users/{user_id}/messages/{message_id}{?select}";
     urlTplParams := make(map[string]string)
     for idx, item := range pathParameters {
         urlTplParams[idx] = item
--- msgraph-mail/go/utilities/users/users_request_builder.go
+++ msgraph-mail/go/utilities/users/users_request_builder.go
@@ -20,7 +20,7 @@ type UsersRequestBuilder struct {
 func NewUsersRequestBuilderInternal(pathParameters map[string]string, requestAdapter ida96af0f171bb75f894a4013a6b3146a4397c58f11adb81a2b7cbea9314783a9.RequestAdapter)(*UsersRequestBuilder) {
     m := &UsersRequestBuilder{
     }
-    m.urlTemplate = "https://graph.microsoft.com/v1.0/users";
+    m.urlTemplate = "{+baseurl}/users";
     urlTplParams := make(map[string]string)
     for idx, item := range pathParameters {
         urlTplParams[idx] = item
