3a7d391b4b4f198ea0553c9a713d8cf46f0cb48f
Andrew Omondi <andrew.omondi@microsoft.com>
2021-12-15 15:26:05 +0300

    Rename HttpMethod to MEthod

--- msgraph-mail/dotnet/Users/Item/InferenceClassification/InferenceClassificationRequestBuilder.cs
+++ msgraph-mail/dotnet/Users/Item/InferenceClassification/InferenceClassificationRequestBuilder.cs
@@ -54,7 +54,7 @@ namespace Graphdotnetv4.Users.Item.InferenceClassification {
         /// </summary>
         public RequestInformation CreateDeleteRequestInformation(Action<IDictionary<string, string>> h = default, IEnumerable<IRequestOption> o = default) {
             var requestInfo = new RequestInformation {
-                HttpMethod = HttpMethod.DELETE,
+                HttpMethod = Method.DELETE,
                 UrlTemplate = UrlTemplate,
                 PathParameters = PathParameters,
             };
@@ -70,7 +70,7 @@ namespace Graphdotnetv4.Users.Item.InferenceClassification {
         /// </summary>
         public RequestInformation CreateGetRequestInformation(Action<GetQueryParameters> q = default, Action<IDictionary<string, string>> h = default, IEnumerable<IRequestOption> o = default) {
             var requestInfo = new RequestInformation {
-                HttpMethod = HttpMethod.GET,
+                HttpMethod = Method.GET,
                 UrlTemplate = UrlTemplate,
                 PathParameters = PathParameters,
             };
@@ -92,7 +92,7 @@ namespace Graphdotnetv4.Users.Item.InferenceClassification {
         public RequestInformation CreatePatchRequestInformation(Graphdotnetv4.Models.Microsoft.Graph.InferenceClassification body, Action<IDictionary<string, string>> h = default, IEnumerable<IRequestOption> o = default) {
             _ = body ?? throw new ArgumentNullException(nameof(body));
             var requestInfo = new RequestInformation {
-                HttpMethod = HttpMethod.PATCH,
+                HttpMethod = Method.PATCH,
                 UrlTemplate = UrlTemplate,
                 PathParameters = PathParameters,
             };
