09b690d87f888159d76ddbab5164c78226fd6933
Vincent Biret <vibiret@microsoft.com>
2022-02-04 12:30:19 -0500

    - code-gen: csharp intermediate exception type

--- msgraph-mail/dotnet/Models/Odata/Error/Error.cs
+++ msgraph-mail/dotnet/Models/Odata/Error/Error.cs
@@ -1,10 +1,11 @@
+using Microsoft.Kiota.Abstractions;
 using Microsoft.Kiota.Abstractions.Serialization;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 namespace Graphdotnetv4.Models.Odata.Error {
-    public class Error : Exception, IParsable {
+    public class Error : ApiException, IParsable {
         /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
         public IDictionary<string, object> AdditionalData { get; set; }
         public Main Error_prop { get; set; }
