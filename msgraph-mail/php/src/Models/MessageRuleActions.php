860d5a249640be31377f0dff4b26617a33482b91
Philip Gichuhi <pgichuhi@microsoft.com>
2022-07-14 18:17:23 +0300

    Update PHP samples

--- msgraph-mail/php/src/Models/MessageRuleActions.php
+++ msgraph-mail/php/src/Models/MessageRuleActions.php
@@ -10,7 +10,7 @@ use Microsoft\Kiota\Abstractions\Serialization\SerializationWriter;
 class MessageRuleActions implements AdditionalDataHolder, Parsable 
 {
     /**
-     * @var array<string, mixed> $AdditionalData Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.
+     * @var array<string, mixed> $additionalData Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.
     */
     private array $additionalData;
     
@@ -73,7 +73,7 @@ class MessageRuleActions implements AdditionalDataHolder, Parsable
      * Instantiates a new messageRuleActions and sets the default values.
     */
     public function __construct() {
-        $this->additionalData = [];
+        $this->setAdditionalData([]);
     }
 
     /**
