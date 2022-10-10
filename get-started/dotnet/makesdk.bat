56d4aab0ee4d09428d68e3a1ebcf80c8eb6202a9
Jason Johnston <jasonjoh@users.noreply.github.com>
2022-10-10 14:07:16 -0400

    Update with latest Kiota builder and dependencies

--- get-started/dotnet/makesdk.bat
+++ get-started/dotnet/makesdk.bat
@@ -1,1 +1,1 @@
-kiota -l CSharp -c GetUserApiClient -n GetUserClient.ApiClient -d ../getme.yml -o ./src/Client
+kiota generate -l CSharp -c GetUserApiClient -n GetUserClient.ApiClient -d ../getme.yml -o ./src/Client
