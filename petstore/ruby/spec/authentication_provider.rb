92598248de0b69354306e444fb38b9b08398450b
Vincent Biret <vibiret@microsoft.com>
2023-04-04 14:52:32 -0400

    - removes ruby and php

--- petstore/ruby/spec/authentication_provider.rb
+++
@@ -1,5 +0,0 @@
-class AuthenticationProvider < MicrosoftKiotaAbstractions::BaseBearerTokenAuthenticationProvider
-  def get_authorization_token(request)
-    return "" # TODO replace with access token
-  end
-end
