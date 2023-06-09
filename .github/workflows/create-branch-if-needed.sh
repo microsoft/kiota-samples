99dba07d68e136dbeec55226a80ef706f1a8c542
Jason Johnston <jasonjoh@users.noreply.github.com>
2023-06-09 17:49:16 -0400

    OK

---
+++ .github/workflows/create-branch-if-needed.sh
@@ -0,0 +1,9 @@
+if [ $(git status --porcelain | wc -l) -eq "0" ]; then
+  echo "  🟢 Git repo is clean."
+else
+  echo "  🔴 Git repo modified."
+  timestamp=$(date +%s)
+  branchName="regen/clients-${timestamp}"
+  git checkout -b $branchName
+  git add --all
+fi
