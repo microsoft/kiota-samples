ec0ae48ba8a7d9ce6410ee7e0758043400d50468
Jason Johnston <jasonjoh@users.noreply.github.com>
2023-06-09 17:51:17 -0400

    Added commit line to script

--- .github/workflows/create-branch-if-needed.sh
+++ .github/workflows/create-branch-if-needed.sh
@@ -1,9 +1,10 @@
 if [ $(git status --porcelain | wc -l) -eq "0" ]; then
-  echo "  🟢 Git repo is clean."
+  echo "🟢 Git repo is clean."
 else
-  echo "  🔴 Git repo modified."
+  echo "🔴 Git repo modified."
   timestamp=$(date +%s)
   branchName="regen/clients-${timestamp}"
   git checkout -b $branchName
   git add --all
+  git commit -a -m "Regenerated API clients with Kiota"
 fi
