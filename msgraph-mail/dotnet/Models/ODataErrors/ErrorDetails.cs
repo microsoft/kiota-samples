4580d9a0f8bc6167afb27307c9d2a641feaddc33
Vincent Biret <vibiret@microsoft.com>
2023-10-12 12:38:47 -0400

    - code-gen: csharp 1.7.0

--- msgraph-mail/dotnet/Models/ODataErrors/ErrorDetails.cs
+++ msgraph-mail/dotnet/Models/ODataErrors/ErrorDetails.cs
@@ -4,38 +4,56 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System;
-namespace Graphdotnetv4.Models {
-    public class Entity : IAdditionalDataHolder, IParsable {
+namespace Graphdotnetv4.Models.ODataErrors {
+    public class ErrorDetails : IAdditionalDataHolder, IParsable {
         /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
         public IDictionary<string, object> AdditionalData { get; set; }
-        /// <summary>The unique identifier for an entity. Read-only.</summary>
+        /// <summary>The code property</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
-        public string? Id { get; set; }
+        public string? Code { get; set; }
 #nullable restore
 #else
-        public string Id { get; set; }
+        public string Code { get; set; }
+#endif
+        /// <summary>The message property</summary>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+#nullable enable
+        public string? Message { get; set; }
+#nullable restore
+#else
+        public string Message { get; set; }
+#endif
+        /// <summary>The target property</summary>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+#nullable enable
+        public string? Target { get; set; }
+#nullable restore
+#else
+        public string Target { get; set; }
 #endif
         /// <summary>
-        /// Instantiates a new entity and sets the default values.
+        /// Instantiates a new ErrorDetails and sets the default values.
         /// </summary>
-        public Entity() {
+        public ErrorDetails() {
             AdditionalData = new Dictionary<string, object>();
         }
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static Entity CreateFromDiscriminatorValue(IParseNode parseNode) {
+        public static ErrorDetails CreateFromDiscriminatorValue(IParseNode parseNode) {
             _ = parseNode ?? throw new ArgumentNullException(nameof(parseNode));
-            return new Entity();
+            return new ErrorDetails();
         }
         /// <summary>
         /// The deserialization information for the current model
         /// </summary>
         public IDictionary<string, Action<IParseNode>> GetFieldDeserializers() {
             return new Dictionary<string, Action<IParseNode>> {
-                {"id", n => { Id = n.GetStringValue(); } },
+                {"code", n => { Code = n.GetStringValue(); } },
+                {"message", n => { Message = n.GetStringValue(); } },
+                {"target", n => { Target = n.GetStringValue(); } },
             };
         }
         /// <summary>
@@ -44,7 +62,9 @@ namespace Graphdotnetv4.Models {
         /// <param name="writer">Serialization writer to use to serialize this model</param>
         public void Serialize(ISerializationWriter writer) {
             _ = writer ?? throw new ArgumentNullException(nameof(writer));
-            writer.WriteStringValue("id", Id);
+            writer.WriteStringValue("code", Code);
+            writer.WriteStringValue("message", Message);
+            writer.WriteStringValue("target", Target);
             writer.WriteAdditionalData(AdditionalData);
         }
     }
--- msgraph-mail/dotnet/Models/ODataErrors/InnerError.cs
+++ msgraph-mail/dotnet/Models/ODataErrors/InnerError.cs
@@ -4,38 +4,32 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System;
-namespace Graphdotnetv4.Models {
-    public class Entity : IAdditionalDataHolder, IParsable {
+namespace Graphdotnetv4.Models.ODataErrors {
+    /// <summary>
+    /// The structure of this object is service-specific
+    /// </summary>
+    public class InnerError : IAdditionalDataHolder, IParsable {
         /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
         public IDictionary<string, object> AdditionalData { get; set; }
-        /// <summary>The unique identifier for an entity. Read-only.</summary>
-#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
-#nullable enable
-        public string? Id { get; set; }
-#nullable restore
-#else
-        public string Id { get; set; }
-#endif
         /// <summary>
-        /// Instantiates a new entity and sets the default values.
+        /// Instantiates a new InnerError and sets the default values.
         /// </summary>
-        public Entity() {
+        public InnerError() {
             AdditionalData = new Dictionary<string, object>();
         }
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static Entity CreateFromDiscriminatorValue(IParseNode parseNode) {
+        public static InnerError CreateFromDiscriminatorValue(IParseNode parseNode) {
             _ = parseNode ?? throw new ArgumentNullException(nameof(parseNode));
-            return new Entity();
+            return new InnerError();
         }
         /// <summary>
         /// The deserialization information for the current model
         /// </summary>
         public IDictionary<string, Action<IParseNode>> GetFieldDeserializers() {
             return new Dictionary<string, Action<IParseNode>> {
-                {"id", n => { Id = n.GetStringValue(); } },
             };
         }
         /// <summary>
@@ -44,7 +38,6 @@ namespace Graphdotnetv4.Models {
         /// <param name="writer">Serialization writer to use to serialize this model</param>
         public void Serialize(ISerializationWriter writer) {
             _ = writer ?? throw new ArgumentNullException(nameof(writer));
-            writer.WriteStringValue("id", Id);
             writer.WriteAdditionalData(AdditionalData);
         }
     }
