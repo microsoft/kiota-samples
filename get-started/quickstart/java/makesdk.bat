116955cd41b01b49b37999f85919368b04c2be9c
Vincent Biret <vibiret@microsoft.com>
2024-09-16 09:13:46 -0400

    fix: java generation scripts to avoid differences

--- get-started/quickstart/java/makesdk.bat
+++ get-started/quickstart/java/makesdk.bat
@@ -1,1 +1,1 @@
-kiota generate -l java -c PostsClient -n kiotaposts.client -d ../posts-api.yml -o ./app/src/main/java/kiotaposts/client
+kiota generate -l java -c PostsClient -n kiotaposts.client -d ../posts-api.yml -o ./app/src/main/java/kiotaposts/client -s none --ds none
