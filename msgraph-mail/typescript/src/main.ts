069247572ff40d40e6db931d8c9552809c0e8bc9
Vincent Biret <vibiret@microsoft.com>
2024-11-09 17:18:41 -0500

    feat: adds msgraph mail typescript main logic
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- msgraph-mail/typescript/src/main.ts
+++ msgraph-mail/typescript/src/main.ts
@@ -1,9 +1,44 @@
-import { App } from './app/app';
-import { Logger } from './app/common/logger';
+import { Logger } from "./app/common/logger.js";
+import { DeviceCodeCredential } from "@azure/identity";
+import { AzureIdentityAuthenticationProvider } from "@microsoft/kiota-authentication-azure";
+import {
+  FetchRequestAdapter,
+  HttpClient,
+} from "@microsoft/kiota-http-fetchlibrary";
+import { createApiClient } from "./apiClient.js";
+import {
+  JsonParseNodeFactory,
+  JsonSerializationWriterFactory,
+} from "@microsoft/kiota-serialization-json";
 
-Logger.logTask('SYSTEM', 'STARTING');
-
-App.run().then(() => {
-
-  Logger.logTask('SYSTEM', 'FINISHED');
-});
+async function main(): Promise<void> {
+  const cred = new DeviceCodeCredential({
+    tenantId: "YOUR TENANT ID",
+    clientId: "YOUR CLIENT ID",
+    userPromptCallback: (deviceCodeInfo) => {
+      // eslint-disable-next-line no-console
+      console.log(deviceCodeInfo.message);
+    },
+  });
+  const authProvider = new AzureIdentityAuthenticationProvider(cred, [
+    "Mail.Read",
+  ]);
+  const requestAdapter = new FetchRequestAdapter(
+    authProvider,
+    new JsonParseNodeFactory(),
+    new JsonSerializationWriterFactory(),
+    new HttpClient(),
+    {
+      includeEUIIAttributes: true,
+    }
+  );
+  const client = createApiClient(requestAdapter);
+  const result = await client.users
+    .byUserId("TARGET USER EMAIL")
+    .messages.get();
+  for (const message of result?.value!) {
+    Logger.logTask(`${message.subject}`);
+  }
+  Logger.logTask("hello world");
+}
+main();
