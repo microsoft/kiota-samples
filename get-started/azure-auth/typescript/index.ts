a0e528818463beefebcb91e0b84c0dd053f423a2
Vincent Biret <vibiret@microsoft.com>
2024-11-09 17:32:13 -0500

    fix: azure quick start ts sample
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- get-started/azure-auth/typescript/index.ts
+++ get-started/azure-auth/typescript/index.ts
@@ -2,27 +2,31 @@
 // Licensed under the MIT License.
 
 // <ProgramSnippet>
-import { DeviceCodeCredential } from '@azure/identity';
-import { AzureIdentityAuthenticationProvider } from '@microsoft/kiota-authentication-azure';
-import { FetchRequestAdapter } from '@microsoft/kiota-http-fetchlibrary';
-import { createGetUserApiClient } from './client/getUserApiClient';
+import { DeviceCodeCredential } from "@azure/identity";
+import { AzureIdentityAuthenticationProvider } from "@microsoft/kiota-authentication-azure";
+import { FetchRequestAdapter } from "@microsoft/kiota-http-fetchlibrary";
+import { createGetUserApiClient } from "./client/getUserApiClient.js";
 
-const clientId = 'YOUR_CLIENT_ID';
+const clientId = "YOUR_CLIENT_ID";
 
 // The auth provider will only authorize requests to
 // the allowed hosts, in this case Microsoft Graph
-const allowedHosts = new Set<string>([ 'graph.microsoft.com' ]);
-const graphScopes = [ 'User.Read' ];
+const allowedHosts = new Set<string>(["graph.microsoft.com"]);
+const graphScopes = ["User.Read"];
 
 const credential = new DeviceCodeCredential({
   clientId: clientId,
   userPromptCallback: (deviceCodeInfo) => {
     console.log(deviceCodeInfo.message);
-  }
+  },
 });
 
-const authProvider =
-  new AzureIdentityAuthenticationProvider(credential, graphScopes, undefined, allowedHosts);
+const authProvider = new AzureIdentityAuthenticationProvider(
+  credential,
+  graphScopes,
+  undefined,
+  allowedHosts
+);
 const adapter = new FetchRequestAdapter(authProvider);
 
 const client = createGetUserApiClient(adapter);
